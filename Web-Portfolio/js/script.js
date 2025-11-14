9ab54e73cc90c9008f23f8cd98724e6d8ac5bbf0
Gomez, Joel <jgomez1@student.scitech.edu>
2025-11-14 13:40:03 -0800

    updated js file

--- Web-Portfolio/js/script.js
+++ Web-Portfolio/js/script.js
@@ -1,6 +1,5 @@
 const submitButton = document.querySelector('form button[type="submit"]');
 
 submitButton.addEventListener('click', function (event) {\
-
   alert("Thank you for your message!");
 });
